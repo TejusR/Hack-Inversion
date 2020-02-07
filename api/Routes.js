9806f3fccb299ef53e8f1f002849d9396bce461f
shohan <shohanduttaroy99@gmail.com>
2020-02-07 23:19:39 +0530

    [feat] Add prettier

--- api/Routes.js
+++ api/Routes.js
@@ -1,10 +1,10 @@
-var express = require('express')
-var router = express.Router()
+var express = require('express');
+var router = express.Router();
 
 // middleware that is specific to this router
 
-router.get('/login', function (req, res) {
-  res.send('login')
-})
+router.get('/login', function(req, res) {
+    res.send('login');
+});
 
 module.exports = router;
--- api/app.js
+++ api/app.js
@@ -1,19 +1,21 @@
-const express = require('express')
+const express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 const app = express();
-var routes=require("./Routes");
+var routes = require('./Routes');
 
-app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cors({
-	credentials: true,
-}));
-app.use(routes)
+app.use(
+    cors({
+        credentials: true
+    })
+);
+app.use(routes);
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!');
 });
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+    console.log('Example app listening on port 8000!');
 });
