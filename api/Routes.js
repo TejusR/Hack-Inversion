0a691025cfd5bff9c7a39f1293d40a20a60d1360
TejusR <rameshtejus@gmail.com>
2020-02-07 23:16:51 +0530

    Add Routes file

---
+++ api/Routes.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var router = express.Router()
+
+// middleware that is specific to this router
+
+router.get('/login', function (req, res) {
+  res.send('login')
+})
+
+module.exports = router;
--- api/app.js
+++ api/app.js
@@ -1,6 +1,15 @@
 const express = require('express')
+var bodyParser = require('body-parser');
+var cors = require('cors');
 const app = express();
+var routes=require("./Routes");
 
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(cors({
+	credentials: true,
+}));
+app.use(routes)
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
