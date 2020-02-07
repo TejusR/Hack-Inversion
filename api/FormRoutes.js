6551a6d1daaafad700870504f3e383be841c820e
TejusR <rameshtejus@gmail.com>
2020-02-08 04:10:52 +0530

    FormRoutes.js initial commit

---
+++ api/FormRoutes.js
@@ -0,0 +1,9 @@
+const imaps = require('imap-simple');
+const jwt = require('jsonwebtoken');
+var express = require('express');
+var router = express.Router();
+var models = require('./db/models');
+
+router.get("/getForms",(req,res)=>{
+    
+});
